0f40317acf1256d772794d16f3a1c40d5c923106
Mary Mazi <mazi.mary.o@gmail.com>
2017-06-06 02:40:49 +0100

    Refactor app code to pass available tests

---
+++ app/index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = {
+    getPrimes: (n) => {
+        //isPrime checks if a number is prime
+        let isPrime = (number) => {
+            for (let i = 2; i <= Math.sqrt(number); i++) {
+                if (number % i === 0) {
+                return false;
+                }  
+            }  
+            return true;
+        };
+        if (n <= 0 || typeof n !== 'number') {
+            return ('invalid argument');
+        }
+        else {
+            let primeList = [];
+            for (var j = 2; j <= n; j++) {
+                if (isPrime(j)) {
+                    primeList.push(j);
+                } 
+            }
+            return primeList;
+        }    
+    }
+} 
